0b7e117308ad75bbd5605826f34bb6da6099d9d8
Nataliia Podpriatova <Podpriatova@medialesson.de>
2024-09-05 13:55:31 +0200

    fix: secondary menu tests

--- tests/secondary-menu.spec.ts
+++ tests/secondary-menu.spec.ts
@@ -3,13 +3,14 @@ import { expect, test } from '@playwright/test';
 test(`Should display the secondary menu`, async ({ page }) => {
     await page.goto('/');
 
-    const secondaryNavigation = page.getByRole('navigation', { name: 'Secondary' });
-    const menuItem = secondaryNavigation.getByRole('menuitem', { name: 'Über uns' });
-    await expect(menuItem).toBeVisible();
-    await expect(menuItem.getByText('Über uns')).toBeVisible();
-    await expect(menuItem.getByText('Erfahren Sie wer wir sind uns was uns antreibt.')).toBeVisible();
+    const menu = page.getByRole('list', { name: 'Zusätzliches Menü' });
+    const menuItems = menu.getByRole('link', { name: 'Über uns' });
 
-    menuItem.click();
+    await expect(menu).toBeVisible();
+    await expect(menuItems).toBeVisible();
+    await expect(menuItems.getByText('Erfahren Sie wer wir sind uns was uns antreibt.')).toBeVisible();
+
+    await menuItems.click();
 
     await expect(page).toHaveURL('/about/');
 });

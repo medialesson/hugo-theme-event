2abe4f84143c09b02e275dc497927e05ad141e03
NataliiaPodp <136790795+NataliiaPodp@users.noreply.github.com>
2024-09-24 07:50:54 +0200

    feat: use header menu also with long event titles
    #41

--- tests/baseof.spec.ts
+++ tests/baseof.spec.ts
@@ -3,11 +3,17 @@ import { expect, test } from '@playwright/test';
 test('Should use event title as page title', async ({ page }) => {
     await page.goto('/');
 
-    await expect(page).toHaveTitle(/testcon/);
+    await expect(page).toHaveTitle(/test conference/);
 });
 
 test('Should provide link to home page', async ({ page }) => {
     await page.goto('/');
 
-    await expect(page.getByRole('link', { name: /Testcon/ })).toHaveCount(2);
+    await expect(page.getByRole('link', { name: /Test conference/ })).toHaveCount(1);
+});
+
+test('Should use short event title as a link to home page in the header menu', async ({ page }) => {
+    await page.goto('/');
+
+    await expect(page.getByRole('link', { name: /Testcon/ })).toHaveCount(1);
 });
